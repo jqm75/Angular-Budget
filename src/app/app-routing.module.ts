8cc50b892dea7a5d0a8b549ea5df651e436cc617
Quim <joaquimpujol@gmail.com>
2023-02-19 23:21:05 +0100

    EX6 - Add Budget-List

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './budget/welcome/welcome-page.component';
 import { MainComponent } from './budget/main/main.component';
 import { ModalComponent } from './_modal/modal.component';
+import { BudgetListComponent } from './budget-list/budget-list.component';
 
 const routes: Routes = [
 
   { path: 'welcome-page', component: WelcomePageComponent, pathMatch: 'full' },
   { path: 'main', component: MainComponent },
   { path: 'modal', component: ModalComponent},
+  { path: 'budget-list', component: BudgetListComponent},
   { path: '**', redirectTo: 'welcome-page'}
 
 ];
--- src/app/budget/interfaces/budget.interface.ts
+++ src/app/budget/interfaces/budget.interface.ts
@@ -1,6 +1,11 @@
 export interface Budget {
 
-  pages : number,
-  languages  : number
+  pages      : number,
+  languages  : number,
+  id         : number,
+  budgetName : string,
+  clientName : string,
+  date       : Date,
+  total      : number
 
 }
